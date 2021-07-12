47acb48dba8a66715374b8189e2fca443f2be2d1
[Hector] <[hectorsanzr94@gmail.com]>
2021-07-11 19:42:00 -0500

    TodoFinal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 //import { saludar } from './js/componentes';
 import './styles.css';
 
-import {Todo, TodoList } from './classes/index.js'
+import {Todo, TodoList } from './classes'
 import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
 
+todoList.todos.forEach( crearTodoHtml);
 
+todoList.todos[0].imprimirClase();
 
-
-  todoList.todos.forEach( crearTodoHtml);
+console.log('todos', todoList.todos);
